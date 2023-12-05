c5636bd2de271695d9365842e0479c6b11512de4
Devorah Gertler <118658262+devorahgertler@users.noreply.github.com>
2023-12-05 11:03:55 +0200

    Milestone 3.1 done

---
+++ AllNotes.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { NotesForm } from './NotesForm';
+import Note from './Note';
+import { v4 as uuidv4 } from 'uuid';
+
+uuidv4();
+
+export const AllNotes = () => {
+    const [notes, setNotes] = useState([])
+
+    const month = new Date().toLocaleString('default', { month: 'short' });
+    const date = new Date().getDate();
+    const time = new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+    const addNote = (note, title) => {
+        setNotes([...notes, { title: title, note: note, created: (month + ' ' + date + ", " + time), id: uuidv4() }])
+    }
+
+    return (
+
+        <div>
+            <NotesForm addNote={addNote} />
+            {notes.map((note, id) => (<Note note={note} key={id} notes={notes} />))}
+        </div>
+
+    )
+
+}
---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { AllNotes } from './components/AllNotes.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <AllNotes />
+    </div>
+  );
+}
+
+export default App;
