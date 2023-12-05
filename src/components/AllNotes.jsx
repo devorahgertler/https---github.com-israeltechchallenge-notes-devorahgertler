33ed3dcfaf8427ec5754431efbfe208e9dd111e7
devorahgertler <118658262+devorahgertler@users.noreply.github.com>
2023-12-05 09:30:36 +0200

    Milestone 2

--- src/components/AllNotes.jsx
+++ src/components/AllNotes.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NotesForm } from './NotesForm';
 import Note from './Note';
+import { v4 as uuidv4 } from 'uuid';
 
+uuidv4();
 
 export const AllNotes = () => {
     const [notes, setNotes] = useState([])
+
+    const month = new Date().toLocaleString('default', { month: 'short' });
+    const date = new Date().getDate();
+    const time = new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
     const addNote = (note) => {
-        setNotes([...notes, { note: note, created: (new Date().toLocaleString()) }])
+        setNotes([...notes, { note: note, created: (month + ' ' + date + ", " + time), id: uuidv4() }])
     }
 
     return (
+
         <div>
             <NotesForm addNote={addNote} />
-            {notes.map((note, index) => (<Note note={note} key={index} />))}
+            {notes.map((note, id) => (<Note note={note} key={id} notes={notes} />))}
         </div>
+
     )
+
 }
